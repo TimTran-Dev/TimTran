095405dded2d9916bd713a2f64d9c83f395e4419
TimTran-Dev <timv.tran.dev@gmail.com>
2021-02-23 13:12:16 -0500

    TimTran

---
+++ rps/js/app.js
@@ -0,0 +1,55 @@
+var botScore=0, playerScore=0;
+document.getElementById("rock").onclick=playerThrowsRock;
+function playerThrowsRock(){
+    var botsWeapon= getRandomWeapon();
+    checkWhoWon(botsWeapon,"rock");
+}
+document.getElementById("scissors").onclick=playerThrowsScissors;
+function playerThrowsScissors(){
+    var botsWeapon= getRandomWeapon();
+    checkWhoWon(botsWeapon,"scissors");
+}
+document.getElementById("paper").onclick=playerThrowsPaper;
+function playerThrowsPaper(){
+    var botsWeapon= getRandomWeapon();
+    checkWhoWon(botsWeapon,"paper");
+}
+function getRandomWeapon(){
+    var randomNumber=Math.random();
+    botsWeapon="rock";
+    if(randomNumber<.33){
+        botsWeapon="scissors";
+    }
+    else if(randomNumber<.6666){
+        botsWeapon="paper";
+    }
+    return botsWeapon;
+}
+function checkWhoWon(botsWeapon,playersWeapon){
+    if(botsWeapon==playersWeapon){
+        displayCompleteMessage("It is a draw!");
+    }
+    else if(
+        (botsWeapon=="scissors" && playersWeapon=="paper") ||
+        (botsWeapon=="paper" && playersWeapon=="rock") ||
+        (botsWeapon=="rock" && playersWeapon=="scissors")
+        ){
+        increaseBotScore();
+    }
+    else{
+        increasePlayerScore();
+    }
+}
+function increaseBotScore(){
+    botScore+=1;
+    document.getElementById("computerScore").innerHTML=botScore;
+    displayCompleteMessage("You have been defeated...");
+}
+function increasePlayerScore(){
+    playerScore+=1;
+    document.getElementById("humanScore").innerHTML=playerScore;
+    displayCompleteMessage("You are the champion!")
+}
+function displayCompleteMessage(msg){
+    document.getElementById("status").innerHTML=msg;
+}
